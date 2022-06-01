fa8e373fa14d0b34227e07ab293837ca9c7c131d
Anshumal Kumarage <anshuwa@gmail.com>
2022-06-01 08:52:09 +0530

    Add project files.

---
+++ SMS_API_CALLER/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_API_CALLER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
